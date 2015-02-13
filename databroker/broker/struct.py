be47e786cac4b3b412711e592b604a6a9ddfd9a7
danielballan <daniel.b.allan@gmail.com>
2015-02-13 17:32:15 -0500

    ENH: Add time_as_datetime for debugging.

--- databroker/broker/struct.py
+++ databroker/broker/struct.py
@@ -19,6 +19,9 @@ class BrokerStruct(object):
             if isinstance(attr, mongoengine.Document):
                 attr = BrokerStruct(attr)
             setattr(self, field, attr)
+            # For debugging, add a human-friendly time_as_datetime attribute.
+                if hasattr(self, 'time'):
+                    self.time_as_datetime = datetime.datetime.fromtimestamp(time)
 
     def __repr__(self):
         return "<BrokerStruct {0}>".format(self._name)
